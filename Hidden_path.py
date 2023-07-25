996aeaeb5812f23a430f3adacad0ec7f88ed9203
tania003 <134498104+tania003@users.noreply.github.com>
2023-07-25 01:02:20 -0400

    Add files via upload

---
+++ Hidden_path.py
@@ -0,0 +1,18 @@
+import requests
+
+target_url = "travelpointbus.com"
+
+
+def request(url):
+    try:
+      return requests.get("http://" + url)
+    except requests.exceptions.ConnectionError:
+      pass
+
+with open("/home/kali/Downloads/common.txt","r") as wordlist_file:
+ for line in wordlist_file:
+  word = line.strip()
+  test_url = target_url + "/" + word
+  response = request(test_url)
+  if response:
+    print("[+] Discovered URL ---->" + test_url)
